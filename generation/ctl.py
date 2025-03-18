26278f1459f5dde60f3fc612f55b8c183b306402
ST2SHY <ST2SHY@proton.me>
2025-03-18 16:43:14 +0800

    add generation/

---
+++ generation/ctl.py
@@ -0,0 +1,21 @@
+# Generate CTL
+
+'''
+% Please generate a ${description} following the requirements provided below:
+
+% ${ requirements: [A, B, ...] }
+
+% For each requirement, translate it into a CTL (Computation Tree Logic) formula.
+% Start by assigning a unique symbol to each activity or action, and then
+% represent the requirements using CTL formulas.
+
+% The output should be structured as follows:
+
+% { 
+%     symbols: [(initiator, symbol, activity)],
+%     formulas:[CTL_formula_1, CTL_formula_2, ...]
+% }
+
+% Make sure each CTL formula accurately captures the corresponding requirement.
+% 
+'''
